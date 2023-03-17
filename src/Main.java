419a098e5970e7933aeca73ff7801c764e525895
alejo <alejocrrs@gmail.com>
2023-03-17 18:46:42 -0500

    Create MenuCursos & MenuSalones

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        LinkedList<Curso> cursos = new LinkedList<>();
         LinkedList<Asignatura> asignaturas = new LinkedList<>();
         LinkedList<Docente> docentes = new LinkedList<>();
         LinkedList<Salon> salones = new LinkedList<>();
-        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        MenuPrincipal menuPrincipal = new MenuPrincipal(cursos, asignaturas, docentes, salones);
     }
 }
